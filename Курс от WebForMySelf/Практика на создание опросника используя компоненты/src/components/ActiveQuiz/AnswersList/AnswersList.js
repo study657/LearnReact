59f05ce706fa94c6bb0778cb32219b2e9f6ec2ff
study657 <79162199605@list.ru>
2020-11-10 01:05:14 +0300

    Практика на создание опросника используя компоненты

---
+++ Курс от WebForMySelf/Практика на создание опросника используя компоненты/src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from './AnswersList.module.css';
+import AnswerItem from './AnswerItem/AnswerItem.js';
+
+const AnswersList = props => {
+    return (
+        <ul className={classes.AnswersList}>
+            {props.answers.map((answer, index) => {
+                return (
+                    <AnswerItem key={index} answer={answer} />
+                )
+            })}
+        </ul>
+    );
+};
+
+export default AnswersList;
