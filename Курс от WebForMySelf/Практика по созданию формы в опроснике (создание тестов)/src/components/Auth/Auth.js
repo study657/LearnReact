853938cbd7fa09619a0cac44e79d94ffb43de12d
study657 <79162199605@list.ru>
2020-12-03 00:29:01 +0300

    Практика по созданию формы в опроснике (создание тестов)

---
+++ Курс от WebForMySelf/Практика по созданию формы в опроснике (создание тестов)/src/components/Auth/Auth.js
@@ -0,0 +1,126 @@
+import React, {Component} from 'react';
+import classes from './Auth.module.css';
+import Button from '../../components/Ui/Button/Button.js';
+import Input from '../../components/Ui/Input/Input.js';
+
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+};
+
+class Auth extends Component{
+    state = {
+        isFormValid: false,
+        formControls: {
+            email: {
+                value: '',
+                type: 'email',
+                label: 'Email',
+                errorMessage: 'Введите корректный email',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    email: true
+                }
+            },
+            password: {
+                value: '',
+                type: 'password',
+                label: 'Пароль',
+                errorMessage: 'Введите корректный пароль',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    minLength: 6
+                }
+            }
+        }
+    }
+
+    loginHandler = () => {
+
+    };
+
+    registerHandler = () => {
+
+    };
+
+    submitHandler = event => {
+        event.preventDefault();
+    };
+
+    validateControl(value, validation){
+        if(!validation){
+            return true;
+        }
+
+        let isValid = true;
+
+        if(validation.required){
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if(validation.email){
+            isValid = validateEmail(value) && isValid;
+        }
+
+        if(validation.minLength){
+            isValid = value.length >= validation.minLength && isValid;
+        }
+
+        return isValid;
+    };
+
+    onChangeHandler = (event, controlName) => {
+        const formControls = {...this.state.formControls};
+        const control      = {...formControls[controlName]};
+
+        control.value = event.target.value;
+        control.touched = true;
+        control.valid   = this.validateControl(control.value, control.validation);
+
+        formControls[controlName] = control;
+
+        let isFormValid = true;
+
+        Object.keys(formControls).forEach(name => {
+            isFormValid = formControls[name].valid && isFormValid;
+        });
+
+        this.setState({
+            formControls, isFormValid
+        });
+    };
+
+    renderInputs(){
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName];
+
+            return (
+                <Input key={controlName + index} type={control.type} value={control.value} valid={control.valid} touched={control.touched} label={control.label} shouldValidate={!!control.validation} errorMessage={control.errorMessage} onChange={event => this.onChangeHandler(event, controlName)} />
+            );
+        });
+    };
+
+    render(){
+        return (
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+
+                    <form className={classes.AuthForm} onSubmit={this.submitHandler}>
+                        {this.renderInputs()}
+
+                        <br/>
+                        <Button type="success" disabled={!this.state.isFormValid} onClick={this.loginHandler}>Войти</Button>
+                        <Button type="primary" disabled={!this.state.isFormValid} onClick={this.registerHandler}>Зарегистрироваться</Button>
+                    </form>
+                </div>
+            </div>
+        );
+    };
+};
+
+export default Auth;
---
+++ Курс от WebForMySelf/Практика по созданию формы в опроснике (создание тестов)/src/hoc/Layout/Auxililary/Auxililary.js
@@ -0,0 +1,3 @@
+const Auxililary = props => props.children;
+
+export default Auxililary;
