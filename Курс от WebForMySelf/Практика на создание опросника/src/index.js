053ef2dd3d107ed40ae8e7e3ddd657908a83be09
study657 <79162199605@list.ru>
2020-12-07 22:02:09 +0300

    Практика по React Router

--- Курс от WebForMySelf/Практика на создание опросника/src/index.js
+++ Курс от WebForMySelf/Практика на создание опросника/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
+
+const aplication = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {aplication}
   </React.StrictMode>,
   document.getElementById('root')
 );
