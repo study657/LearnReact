30052dfd4250119dabd253f7e70a0f1b7220572c
study657 <79162199605@list.ru>
2020-12-07 21:59:56 +0300

    Практика на создание опросника используя компоненты

---
+++ Курс от WebForMySelf/Практика на создание опросника/src/hoc/Layout/Layout.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import classes from './Layout.module.css';
+import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle.js';
+import Drawer from '../../components/Navigation/Drawer/Drawer.js';
+
+class Layout extends Component {
+    state = {menu: false};
+
+    toggleMenuHandler = () => {
+        this.setState({menu: !this.state.menu});
+    };
+
+    menuCloseHandler = () => {
+        this.setState({
+            menu: false
+        });
+    };
+
+    render(){
+        return (
+            <div className={classes.Layout}>
+
+                <Drawer isOpen={this.state.menu} onClose={this.menuCloseHandler} />
+
+                <MenuToggle onToggle={this.toggleMenuHandler} isOpen={this.state.menu} />
+                
+                <main>
+                    {this.props.children}
+                </main>
+            </div>
+        );
+    };
+};
+
+export default Layout;
