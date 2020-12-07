916f0add6c8e51c5ee7cf91070d450a5427ad2f2
study657 <79162199605@list.ru>
2020-12-07 22:07:42 +0300

    Практика по созданию формы в опроснике (создание тестов)

--- Курс от WebForMySelf/Практика на создание опросника/src/App.js
+++ Курс от WebForMySelf/Практика на создание опросника/src/App.js
@@ -12,7 +12,7 @@ class App extends Component{
       <Layout>
         <Switch>
           <Route path="/auth" component={Auth} />
-          <Route path="quiz-creactor" component={QuizCreator} />
+          <Route path="/quiz-creator" component={QuizCreator} />
           <Route path="/quiz/:id" component={Quiz} />
           <Route path="/" component={QuizList} />
         </Switch>
