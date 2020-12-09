68e4167849d1c15a9ecaf5eccaf665fae110b3f0
study657 <79162199605@list.ru>
2020-12-09 21:45:23 +0300

    Практика по Redux (переделываем страницу тестов в соответствии с данной технологией)

--- Курс от WebForMySelf/Практика на создание опросника/src/containers/Quiz/Quiz.js
+++ Курс от WebForMySelf/Практика на создание опросника/src/containers/Quiz/Quiz.js
@@ -3,76 +3,17 @@ import classes from './Quiz.module.css';
 import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz.js';
 import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz.js';
 import Loader from '../../components/Ui/Loader/Loader.js'
-import axios from '../../axios/axios-quiz.js';
+import { connect } from 'react-redux'; // Подключаем функцию connect
+import { fetchQuizesById, quizAnswerClick, retryQuiz } from '../../store/actions/quiz';
 
 class Quiz extends Component{
-    state = {
-        results: {},
-        isFinished: false,
-        activeQuestion: 0,
-        answerState: null,
-        quiz: [],
-        loading: true
+
+    componentDidMount(){
+        this.props.fetchQuizesById(this.props.match.params.id);
     };
 
-    onAnswerClickHandler = (answerId) => {
-        if(this.state.answerState){
-            const key = Object.keys(this.state.answerState)[0];
-            if(this.state.answerState[key] === 'success'){
-                return
-            }
-        }
-
-        const question  = this.state.quiz[this.state.activeQuestion];
-        const results   = this.state.results;
-
-        if(question.rightAnswerId === answerId){
-            if(!results[question.id]){
-                results[question.id] = 'success';
-            }
-
-            this.setState({answerState: {[answerId]: 'success', results}});
-
-            const timeout = window.setTimeout(() => {
-                if(this.isQuizFinished()){
-                    this.setState({isFinished: true});
-                }else{
-                    this.setState({activeQuestion: this.state.activeQuestion + 1, answerState: null});
-                }
-
-                window.clearTimeout(timeout);
-            }, 1000);
-        }else{
-            results[question.id] = 'error';
-            this.setState({answerState: {[answerId]: 'error', results}});
-        }
-    };
-
-    isQuizFinished(){
-        return this.state.activeQuestion + 1 === this.state.quiz.length;
-    };
-
-    retryHandler = () => {
-        this.setState({
-            activeQuestion: 0,
-            answerState: null,
-            isFinished: false,
-            results: {}
-        });
-    };
-
-    async componentDidMount(){
-        try{
-            const response = await axios.get(`/quizes/${this.props.match.params.id}.json`);
-            const quiz = response.data;
-
-            this.setState({
-                quiz,
-                loading: false
-            });
-        }catch(e){
-            console.log(e);
-        }
+    componentWillUnmount(){
+        this.props.retryQuiz();
     };
 
     render(){
@@ -82,23 +23,23 @@ class Quiz extends Component{
                     <h1>Ответьте на все вопросы</h1>
 
                     { 
-                        this.state.loading 
+                        this.props.loading || !this.props.quiz
                         ? <Loader />
-                        : this.state.isFinished 
+                        : this.props.isFinished 
                         ? 
                         <FinishedQuiz 
-                            results={this.state.results}
-                            quiz={this.state.quiz}
-                            onRetry={this.retryHandler}
+                            results={this.props.results}
+                            quiz={this.props.quiz}
+                            onRetry={this.props.retryQuiz}
                         />
                         :
                         <ActiveQuiz 
-                            answers={this.state.quiz[this.state.activeQuestion].answers} 
-                            question={this.state.quiz[this.state.activeQuestion].question}
-                            onAnswerClick={this.onAnswerClickHandler}
-                            quizLength={this.state.quiz.length}
-                            answerNumber={this.state.activeQuestion + 1}
-                            state={this.state.answerState}
+                            answers={this.props.quiz[this.props.activeQuestion].answers} 
+                            question={this.props.quiz[this.props.activeQuestion].question}
+                            onAnswerClick={this.props.quizAnswerClick}
+                            quizLength={this.props.quiz.length}
+                            answerNumber={this.props.activeQuestion + 1}
+                            state={this.props.answerState}
                         />
                     }
                 </div>
@@ -107,4 +48,23 @@ class Quiz extends Component{
     };
 };
 
-export default Quiz;
+function mapStateToProps(state){ // Создаем эту функцию для работы
+    return {
+        results: state.quiz.results,         // Эти параметры описаны в начальном стейте
+        isFinished: state.quiz.isFinished,
+        activeQuestion: state.quiz.activeQuestion,
+        answerState: state.quiz.answerState,
+        quiz: state.quiz.quiz,
+        loading: state.quiz.loading
+    }
+};
+
+function mapDispatchToProps(dispatch){ // Создаем эту функцию для работы
+    return { // В этой функции мы будем говорить, что ей нужно загрузить какой-то набор тестов для работы
+        fetchQuizesById: (id) => dispatch(fetchQuizesById(id)), // функции для диспатчев
+        quizAnswerClick: answerId => dispatch(quizAnswerClick(answerId)),
+        retryQuiz: () => dispatch(retryQuiz())
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Quiz);
--- Курс от WebForMySelf/Практика на создание опросника/src/store/reducers/quiz.js
+++ Курс от WebForMySelf/Практика на создание опросника/src/store/reducers/quiz.js
@@ -1,9 +1,15 @@
 // Данный файл будет отвечать за редюсер, который относится к тестам, т.к. state компонента QuizList.js и Quiz.js
-import { FETCH_QUIZES_ERROR, FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS } from '../actions/actionTypes.js';
+import { FETCH_QUIZES_ERROR, FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS, FETCH_QUIZ_SUCCESS, FINISH_QUIZ, QUIZ_NEXT_QUESTION, QUIZ_SET_STATE, QUIZ_RETRY } from '../actions/actionTypes.js';
 const initialState = { // Создается стейт, с которого начинается все наше приложение (state из файла QuizList.js)
     quizes: [],
     loading: false,
-    error: null
+    error: null,
+
+    results: {},
+    isFinished: false,
+    activeQuestion: 0,
+    answerState: null,
+    quiz: null
 };
 
 export default function quizReducer(state = initialState, action){
@@ -19,6 +25,32 @@ export default function quizReducer(state = initialState, action){
         case FETCH_QUIZES_ERROR: // Делаем три проверки под наши переменный
             return {
                 ...state, loading: false, error: action.error
+            }
+
+        case FETCH_QUIZ_SUCCESS:
+            return {
+                ...state, loading: false, quiz: action.quiz
+            }
+        case QUIZ_SET_STATE:
+            return {
+                ...state, answerState: action.answerState, results: action.results
+            }
+
+        case FINISH_QUIZ:
+            return {
+                ...state, isFinished: true
+            }
+        case QUIZ_NEXT_QUESTION:
+            return {
+                ...state, answerState: null, activeQuestion: action.number
+            }
+        case QUIZ_RETRY:
+            return {
+                ...state,
+                activeQuestion: 0,
+                answerState: null,
+                isFinished: false,
+                results: {}
             }
         default:
             return state;
